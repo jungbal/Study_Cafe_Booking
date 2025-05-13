1225c49b9b416c3e55eb7a58daefdaf3fb29d2c4
DESKTOP-BUCI5OD\user1 <hwihun5623@gamail.com>
2025-05-13 17:14:57 +0900

    member 패키지 service dao 정휘훈 파트 추가

--- src/main/java/kr/or/iei/member/model/dao/MemberDao.java
+++ src/main/java/kr/or/iei/member/model/dao/MemberDao.java
@@ -4,9 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import kr.or.iei.common.JDBCTemplate;
 import kr.or.iei.member.model.vo.Member;
+
 
 public class MemberDao {
 	
@@ -68,6 +70,90 @@ public class MemberDao {
 			JDBCTemplate.close(pstmt);
 		}
 
+		return result;
+	}
+	
+	//정휘훈 파트
+	public ArrayList<Member> selectAllUser(Connection conn, int start, int end) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		ArrayList<Member> list = new ArrayList<Member>();
+		
+		String query = "select * from (select ROWNUM RNUM, A.*from ( select * from tbl_user A ) A) where RNUM >=? and RNUM <=?";
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+			
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				Member user = new Member();
+				user.setUserId(rset.getString("user_id"));
+				user.setUserPw(rset.getString("user_pw"));
+				user.setUserRole(rset.getInt("user_role"));
+				user.setUserPhone(rset.getString("user_phone"));
+				user.setUserStatus(rset.getString("user_status"));
+				user.setUserImage(rset.getString("user_image"));
+				
+				list.add(user);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(pstmt);
+		}
+		return list;
+	}
+
+	public int selectTotalCount(Connection conn) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		int totCnt = 0;
+		
+		String query = "select count(*) as cnt from tbl_user";
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			
+			rset = pstmt.executeQuery();
+			rset.next();
+			totCnt = rset.getInt("cnt");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(pstmt);
+		}
+		return totCnt;
+	}
+
+	public int deleteOneUser(Connection conn, String userId) {
+		PreparedStatement pstmt = null;
+		
+		int result = 0;
+		
+		String query = "delete from tbl_user where user_id = ?";
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, userId);
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(conn);
+		}
+		
 		return result;
 	}
 }
