87f4a9c39a4e1d65206a6b63f5cf0008a5a76e60
DESKTOP-JLN2IVQ\user1 <hi09067@gmail.com>
2025-05-13 15:24:08 +0900

    댓글/리뷰 테이블 관련 Dao, Service, vo 생성

---
+++ src/main/java/kr/or/iei/cafe/model/vo/Comment.java
@@ -0,0 +1,21 @@
+package kr.or.iei.cafe.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor 
+@NoArgsConstructor
+@Data
+public class Comment {
+	private String commentId;
+	private String targetType;
+	private String content;
+	private String commentTime;
+	private String commentUserId;
+	private String commentCafeNo;
+	private String commentParent;
+	private String userRole;
+	
+
+}
