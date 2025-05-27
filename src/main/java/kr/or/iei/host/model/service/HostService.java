fef53244a2636d65f219f505c9375d447ab65168
DESKTOP-JLN2IVQ\user1 <hi09067@gmail.com>
2025-05-27 17:38:56 +0900

    updateHostRequestStatus 메소드 삭제

--- src/main/java/kr/or/iei/host/model/service/HostService.java
+++ src/main/java/kr/or/iei/host/model/service/HostService.java
@@ -103,31 +103,37 @@ public class HostService {
 
 	    int result1 = dao.changeCafe(conn, cafe);  // tbl_cafe 업데이트
 	    int result2 = 0;
-	    int result3 = 0;
+	    //int result3 = 0;
 	    int result4 = 0;
 	    int result5 = 0; // 이미지 update 실패 시 rollback 되게 설정
 
 	    if (result1 > 0) {
 	        result2 = dao.updateCafeStatus(conn, cafe.getCafeNo()); // 상태 변경
-	        result3 = dao.updateHostRequestStatus(conn, cafe.getCafeNo()); 
+	        //result3 = dao.updateHostRequestStatus(conn, cafe.getCafeNo()); 
 	    }
 
-	    if (result1 > 0 && result2 > 0 && result3 > 0) {
+	    if (result1 > 0 && result2 > 0) {
 	        result4 = dao.insertHostRequest(conn, cafe.getCafeNo());
 
 	        // 무조건 update만 수행 (이미지가 무조건 존재하므로)
 	        result5 = dao.updateCafeImage(conn, cafe);
 	    }
 
-	    if (result1 > 0 && result2 > 0 && result3 > 0 && result4 > 0 && result5 > 0) {
+	    if (result1 > 0 && result2 > 0 &&  result4 > 0 && result5 > 0) {
 	        JDBCTemplate.commit(conn);
 	    } else {
 	        JDBCTemplate.rollback(conn);
 	    }
+	    
+	    System.out.println("result1 : " + result1);
+	    System.out.println("result2 : " + result2);
+	    //System.out.println("result3 : " + result3);
+	    System.out.println("result4 : " + result4);
+	    System.out.println("result5 : " + result5);
 
 	    JDBCTemplate.close(conn);
 
-	    return (result1 > 0 && result2 > 0 && result3 > 0 && result4 > 0 && result5 > 0) ? 1 : 0;
+	    return (result1 > 0 && result2 > 0 && result4 > 0 && result5 > 0) ? 1 : 0;
 	}
 
 }
